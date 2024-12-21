0a67be2491d6c5f613d49d96944ab95a88a0b1e4
Cássio Reis <csreis04@gmail.com>
2024-12-21 14:37:21 -0300

    Implementa o mergeAndCount

--- 493. Reverse Pairs/493.cpp
+++ 493. Reverse Pairs/493.cpp
@@ -5,11 +5,33 @@ class Solution {
 public:
 
     pair<int, vector<int>> mergeAndCount(vector<int>& numsA, vector<int>& numsB) {
+        int i = 0, j = 0, inversoes = 0;
+        vector<int> merged;
 
+        for (int i = 0; i < numsA.size(); i++) {
+            while (j < numsB.size() && numsA[i] > 2LL * numsB[j]) {
+                j++;
+            }
+            inversoes += j;
+        }
+        
+        i = 0, j = 0;
+        while (i < numsA.size() && j < numsB.size()) {
+            if (numsA[i] <= numsB[j]) {
+                merged.push_back(numsA[i++]);
+            } else {
+                merged.push_back(numsB[j++]);
+            }
+        }
+
+        while (i < numsA.size()) merged.push_back(numsA[i++]);
+        while (j < numsB.size()) merged.push_back(numsB[j++]);
+
+    return {inversoes, merged};
     }
 
     pair<int, vector<int>> sortAndCount(vector<int>& nums) {
-        
+
         if (nums.size() <= 1) {
             return {0, nums};
         }
@@ -27,6 +49,8 @@ public:
     }
 
     int reversePairs(vector<int>& nums) {
-        
+        auto [inversoes, sorted] = sortAndCount(nums);
+
+        return inversoes;
     }
 };
