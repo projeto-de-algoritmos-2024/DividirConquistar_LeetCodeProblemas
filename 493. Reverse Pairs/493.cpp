1e7615b2b71e4bcf8c7613dcbfc257ec8e2add58
Cássio Reis <csreis04@gmail.com>
2024-12-21 15:07:41 -0300

    Adiciona comentários no código da questão 493

--- 493. Reverse Pairs/493.cpp
+++ 493. Reverse Pairs/493.cpp
@@ -7,13 +7,15 @@ public:
         int qtdInversoes = 0;
         int i = l, j = m + 1, k = 0;
 
+        // Contagem de pares reversos: nums[i] > 2 * nums[j]
         for (int i = l; i <= m; i++) {
             while (j <= r && nums[i] > 2LL * nums[j]) {
                 j++;
             }
-            qtdInversoes += j - (m + 1);
+            qtdInversoes += j - (m + 1); // Adiciona o número de inversões
         }
 
+        // Merge dos subvetores
         vector<int> aux(r - l + 1);
         i = l, j = m + 1;
 
@@ -41,8 +43,11 @@ public:
         int m = l + (r - l) / 2;
         int count = 0;
 
+        // Recursivamente divide o vetor e conta os pares reversos em cada metade
         count += sortAndCount(nums, l, m);
         count += sortAndCount(nums, m + 1, r);
+
+        // Conta os pares reversos entre as duas metades ao fazer o merge
         count += mergeAndCount(nums, l, m, r);
 
         return count;
